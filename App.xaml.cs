a727b133cda2de7e78d00e7c2da858e242528954
Sagar1555 <stimalsena155@gmail.com>
2022-09-01 11:56:42 +0545

    first maui

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUI_APP;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUI_APP;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
